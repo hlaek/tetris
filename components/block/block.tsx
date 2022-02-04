6fa3741d3ed5c18da3518fa9ee30fd77f67b7897
Kayla Himmelberger <38771553+kaylahimmel@users.noreply.github.com>
2022-02-04 12:59:45 -0600

    updated BlockColor enums to use CSS variables and added/updated global, block, and app styles

--- components/block/block.tsx
+++ components/block/block.tsx
@@ -7,17 +7,19 @@ interface BlockProps {
 }
 
 export enum BlockColor {
-    purple = 'purple',
-    pink = 'pink', 
-    blue = 'blue', 
-    green = 'green', 
-    orange = 'orange', 
-    white = 'white'
+    white = '--color-0',
+    orange = '--color-1', 
+    yellow = '--color-2', 
+    purple = '--color-3',
+    pink = '--color-4', 
+    green = '--color-5', 
+    blue = '--color-6', 
+    red = '--color-7', 
 }
 
 const Block = ({ blockColor, gridBlocks }: BlockProps) => {
     return (
-        <div className={`${styles.block} ${gridBlocks && styles.gridBlocks}`} style={{backgroundColor: `${blockColor}`}}></div>
+        <div className={`${styles.block} ${gridBlocks && styles.gridBlocks}`} style={{backgroundColor: `var(${blockColor})`}}></div>
     )
 }
 
