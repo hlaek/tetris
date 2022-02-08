80606c1e5f70ea92ca8747b273684bcee5a48ff9
Kayla Himmelberger <38771553+kaylahimmel@users.noreply.github.com>
2022-02-08 15:17:23 -0500

    WIP - started tying on the actions to the controls

--- components/Controls.tsx
+++ components/Controls.tsx
@@ -1,35 +1,46 @@
 import React, {MouseEvent} from 'react';
+import { moveDown, moveLeft, moveRight, rotate } from '../actions';
 // import gameReducer from '../'
 
 export default function Controls() {
 
-  // const handleMouseEvent = (e: MouseEvent<HTMLButtonElement>) => {
-  //   e.preventDefault();
+  const handleMouseEvent = (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
     
-  //   switch (button.id) {
+    switch (e.id) {
+      case id === "left":
+        return moveLeft(e);
 
-  //   }
-  // };
+      case id === "right":
+        return moveRight(e);
+
+      case id === "rotate":
+        return rotate(e);
+
+      case id === "down":
+        return moveDown(e);
+    }
+  };
 
     return (
         <div className="controls">
             {/* left */}
-            <button className="control-button" onClick={(handleMouseEvent) => {
+            <button className="control-button" id="left" onClick={(handleMouseEvent) => {
 
             }}>Left</button>
 
             {/* right */}
-            <button className="control-button" onClick={(handleMouseEvent) => {
+            <button className="control-button" id="right" onClick={(handleMouseEvent) => {
 
             }}>Right</button>
 
             {/* rotate */}
-            <button className="control-button" onClick={(handleMouseEvent) => {
+            <button className="control-button" id="rotate" onClick={(handleMouseEvent) => {
 
             }}>Rotate</button>
 
             {/* down */}
-            <button className="control-button" onClick={(handleMouseEvent) => {
+            <button className="control-button" id="down" onClick={(handleMouseEvent) => {
 
             }}>Down</button>
 
