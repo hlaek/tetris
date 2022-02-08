28bb42419fb43addeb0c0f7df2ca42e7b0620344
Kayla Himmelberger <38771553+kaylahimmel@users.noreply.github.com>
2022-02-08 09:42:34 -0500

    updated interface and component to use JSX.Element for the Scoreboard

--- components/scoreBoard/scoreBoard.tsx
+++ components/scoreBoard/scoreBoard.tsx
@@ -1,21 +1,16 @@
 import React from 'react'
 import styles from './scoreBoard.module.scss'
 
-// interface ScoreBoardProps {
-//     props: {
-//         score?: number;
-//         level?: number;
-//     };
-// }
-// TO-DO: replace any type with interface/better typing
-const ScoreBoard = (
-    props: any,
-) => {
-
+interface ScoreBoardProps {
+    score?: number;
+    level?: number;
+}
+//const App = ({ message }: AppProps): JSX.Element => <div>{message}</div>;
+const ScoreBoard = ({score, level}: ScoreBoardProps): JSX.Element => {
     return (
-        <div className="score-board" {...props}>
-            <div>Score: { props.score }</div>
-            <div>Level: { props.level }</div>
+        <div className="score-board">
+            <div>Score: { score }</div>
+            <div>Level: { level }</div>
             <button className="score-board-button" onClick={(e) => {
             }}>Play</button>
             <button className="score-board-button" onClick={(e) => {
