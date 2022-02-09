8a946a7718f38f315c3e41d9ee74bbfb711248bd
Kayla Himmelberger <38771553+kaylahimmel@users.noreply.github.com>
2022-02-09 16:20:21 -0500

    reordered control buttons and added styles

--- components/Controls.tsx
+++ components/Controls.tsx
@@ -1,8 +1,6 @@
 import React, {ButtonHTMLAttributes, MouseEvent, useContext, useReducer} from 'react';
 import { ActionType, moveDown, moveLeft, moveRight, rotate } from '../actions';
-import gameReducer from '../reducers';
 import { AppContext } from '../context/state';
-
 
 export default function Controls() {
   const { dispatch } = useContext(AppContext);
@@ -14,16 +12,16 @@ export default function Controls() {
   return (
     <div className="controls">
       {/* left */}
-      <button className="control-button" id={ActionType.MOVE_LEFT} onClick={handleSubmitEvent}>Left</button>
-
-      {/* right */}
-      <button className="control-button" id={ActionType.MOVE_RIGHT} onClick={handleSubmitEvent}>Right</button>
+      <button className="control__button" id={ActionType.MOVE_LEFT} onClick={handleSubmitEvent}>Left</button>
 
       {/* rotate */}
-      <button className="control-button" id={ActionType.ROTATE} onClick={handleSubmitEvent}>Rotate</button>
+      <button className="control__button" id={ActionType.ROTATE} onClick={handleSubmitEvent}>Rotate</button>
+
+      {/* right */}
+      <button className="control__button" id={ActionType.MOVE_RIGHT} onClick={handleSubmitEvent}>Right</button>
 
       {/* down */}
-      <button className="control-button" id={ActionType.MOVE_DOWN} onClick={handleSubmitEvent}>Down</button>
+      <button className="control__button" id={ActionType.MOVE_DOWN} onClick={handleSubmitEvent}>Down</button>
     </div>
   )
 }
