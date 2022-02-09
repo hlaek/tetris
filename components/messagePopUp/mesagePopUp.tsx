7e17e63ee52f744e0914dbd6ae5478b1e9f23a42
Hannah Laek <hannahlaek@Hannahs-MacBook-Pro.local>
2022-02-09 09:27:00 -0500

    begin message pop up

---
+++ components/messagePopUp/mesagePopUp.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+// Displays a message
+const MessagePopup: React.FC = (props) => {
+  return (
+    <div className="message-popup">
+      <h1>Message Title</h1>
+      <p>Message info...</p>
+    </div>
+  );
+};
+
+export default MessagePopup;
